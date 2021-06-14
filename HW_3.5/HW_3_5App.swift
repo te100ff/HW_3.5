22ed1e76bc929497b967dcf1af11af5841af4468
Stanislav <te100ff@inbox.ru>
2021-06-14 22:22:50 +0300

    Initial Commit

---
+++ HW_3.5/HW_3_5App.swift
@@ -0,0 +1,17 @@
+//
+//  HW_3_5App.swift
+//  HW_3.5
+//
+//  Created by Stanislav Testov on 14.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct HW_3_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
